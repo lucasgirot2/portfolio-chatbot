d1f9c6be55eb5032c1ba51f51d3197fcd3083c82
lucasgirot2 <lucasgirot2@gmail.com>
2026-05-20 09:27:55 +0200

    Add CORS support for OPTIONS requests

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,17 @@
 export const config = { runtime: 'edge' };
 
 export default async function handler(req) {
+  if (req.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      }
+    });
+  }
+
   if (req.method !== 'POST') {
     return new Response('Method not allowed', { status: 405 });
   }
@@ -38,7 +49,7 @@ export default async function handler(req) {
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type'
       }
     });
